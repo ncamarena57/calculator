91e29fb5687ada903ded90d73a09a98afec24427
Nathan Camarena <59515478+ncamarena57@users.noreply.github.com>
2024-01-09 03:11:52 -0800

    Create operate function

--- script.js
+++ script.js
@@ -15,9 +15,24 @@ function divide(x, y) {
     return x / y;
 }
 
-// Operands and Operators
+// Operand and Operator Variables
 let operand1 = 0;
 let operand2 = 0;
 let operator = "";
 
-
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+    }
+}
