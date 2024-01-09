c6720e99d007d94aa733a9e0cdf2b0f9d74d4a20
Nathan Camarena <59515478+ncamarena57@users.noreply.github.com>
2024-01-09 05:12:35 -0800

    Add number display functionality

---
+++ main.js
@@ -0,0 +1,29 @@
+let currentNum = "";
+let previousNum = "";
+let operator = "";
+
+const currentDisplayNumber = document.querySelector('.currentNumber');
+const previousDisplayNumber = document.querySelector('.previousNumber');
+
+const equal = document.querySelector('.equal');
+
+const decimal = document.querySelector('.decimal');
+
+const clear = document.querySelector('.clear');
+
+const numberButtons = document.querySelectorAll('.number');
+
+const operators = document.querySelectorAll('.operator');
+
+numberButtons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        handleNumber(e.target.textContent);
+    });
+});
+
+function handleNumber(number) {
+    if (currentNum.length <= 11) {
+        currentNum += number;
+        currentDisplayNumber.textContent = currentNum;
+    }
+}
