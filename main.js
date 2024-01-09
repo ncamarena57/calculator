8801a34a9e1a09d823fd90496b58c1581a733c4f
Nathan Camarena <59515478+ncamarena57@users.noreply.github.com>
2024-01-09 05:31:24 -0800

    Add calculate function with addition

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const currentDisplayNumber = document.querySelector('.currentNumber');
 const previousDisplayNumber = document.querySelector('.previousNumber');
 
 const equal = document.querySelector('.equal');
+equal.addEventListener("click", calculate);
 
 const decimal = document.querySelector('.decimal');
 
@@ -37,7 +38,18 @@ operators.forEach(btn => {
 function handleOperator(op) {
     operator = op;
     previousNum = currentNum;
-    previousDisplayNumber.textContent = previousNum;
+    previousDisplayNumber.textContent = previousNum + " " + operator;
     currentNum = "";
     currentDisplayNumber.textContent = "";
+}
+
+function calculate() {
+    previousNum = Number(previousNum);
+    currentNum = Number(currentNum);
+
+    if (operator === "+") {
+        previousNum = previousNum + currentNum;
+    }
+    previousDisplayNumber.textContent = "";
+    currentDisplayNumber.textContent = previousNum;
 }
